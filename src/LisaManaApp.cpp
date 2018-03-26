fb762fe90ee0a55b235199c5c247dcbb6f44ba8f
Wangshu Sun <sunwangshu1993@gmail.com>
2018-03-26 15:27:42 -0400

    tracking skeleton

--- src/LisaManaApp.cpp
+++ src/LisaManaApp.cpp
@@ -1,6 +1,7 @@
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
+#include "Kinect2.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -8,11 +9,33 @@ using namespace std;
 
 class LisaManaApp : public App {
   public:
+	LisaManaApp();
 	void setup() override;
 	void mouseDown( MouseEvent event ) override;
 	void update() override;
 	void draw() override;
+	void shutdown();
+
+private:
+	Kinect2::DeviceRef mDevice;
+	Kinect2::BodyFrame mBodyFrame;
+	ci::Channel8uRef mChannelBodyIndex;
+	ci::Channel16uRef mChannelDepth;
 };
+
+LisaManaApp::LisaManaApp() {
+	mDevice = Kinect2::Device::create();
+	mDevice->start();
+	mDevice->connectBodyEventHandler([&](const Kinect2::BodyFrame frame) {
+		mBodyFrame = frame;
+	});
+	mDevice->connectBodyIndexEventHandler([&](const Kinect2::BodyIndexFrame frame) {
+		mChannelBodyIndex = frame.getChannel();
+	});
+	mDevice->connectDepthEventHandler([&](const Kinect2::DepthFrame frame) {
+		mChannelDepth = frame.getChannel();
+	});
+}
 
 void LisaManaApp::setup()
 {
@@ -28,7 +51,67 @@ void LisaManaApp::update()
 
 void LisaManaApp::draw()
 {
-	gl::clear( Color( 0, 0, 0 ) ); 
+	const gl::ScopedViewport scopedViewport(ivec2(0), getWindowSize());
+	const gl::ScopedMatrices scopedMatrices;
+	const gl::ScopedBlendAlpha scopedBlendAlpha;
+	gl::setMatricesWindow(getWindowSize());
+	gl::clear();
+	gl::color(ColorAf::white());
+	gl::disableDepthRead();
+	gl::disableDepthWrite();
+
+	if (mChannelDepth) {
+		gl::enable(GL_TEXTURE_2D);
+		const gl::TextureRef tex = gl::Texture::create(*Kinect2::channel16To8(mChannelDepth));
+		gl::draw(tex, tex->getBounds(), Rectf(getWindowBounds()));
+	}
+
+	if (mChannelBodyIndex) {
+		gl::enable(GL_TEXTURE_2D);
+
+		auto drawHand = [&](const Kinect2::Body::Hand& hand, const ivec2& pos) -> void
+		{
+			switch (hand.getState()) {
+			case HandState_Closed:
+				gl::color(ColorAf(1.0f, 0.0f, 0.0f, 0.5f));
+				break;
+			case HandState_Lasso:
+				gl::color(ColorAf(0.0f, 0.0f, 1.0f, 0.5f));
+				break;
+			case HandState_Open:
+				gl::color(ColorAf(0.0f, 1.0f, 0.0f, 0.5f));
+				break;
+			default:
+				gl::color(ColorAf(0.0f, 0.0f, 0.0f, 0.0f));
+				break;
+			}
+			gl::drawSolidCircle(pos, 30.0f, 32);
+		};
+
+		gl::pushMatrices();
+		gl::scale(vec2(getWindowSize()) / vec2(mChannelBodyIndex->getSize()));
+		gl::disable(GL_TEXTURE_2D);
+		for (const Kinect2::Body &body : mBodyFrame.getBodies()) {
+			if (body.isTracked()) {
+				gl::color(ColorAf::white());
+				for (const auto& joint : body.getJointMap()) {
+				//	console() << joint.first << endl;
+					if (joint.second.getTrackingState() == TrackingState::TrackingState_Tracked) {
+						vec2 pos(mDevice->mapCameraToDepth(joint.second.getPosition()));
+						gl::drawSolidCircle(pos, 5.0f, 32);
+						vec2 parent(mDevice->mapCameraToDepth(body.getJointMap().at(joint.second.getParentJoint()).getPosition()));
+						gl::drawLine(pos, parent);
+					}
+				}
+				drawHand(body.getHandLeft(), mDevice->mapCameraToDepth(body.getJointMap().at(JointType_HandLeft).getPosition()));
+				drawHand(body.getHandRight(), mDevice->mapCameraToDepth(body.getJointMap().at(JointType_HandRight).getPosition()));
+			}
+		}
+	}
+}
+
+void LisaManaApp::shutdown() {
+	mDevice->stop();
 }
 
 CINDER_APP( LisaManaApp, RendererGl )
