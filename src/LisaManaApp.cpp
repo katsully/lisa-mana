d2d911b65b6d380e226639a1d2bc592906c6ca76
ROTHBERG <sarahrothberg@gmail.com>
2018-03-27 14:03:15 -0400

    added parameters

--- src/LisaManaApp.cpp
+++ src/LisaManaApp.cpp
@@ -2,6 +2,7 @@
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
 #include "Kinect2.h"
+#include "cinder/params/Params.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -13,7 +14,6 @@ class LisaManaApp : public App {
 	void setup() override;
 	void mouseDown( MouseEvent event ) override;
 	void update() override;
-	void drawPentagon(map <JointType, Kinect2::Body::Joint> jointMap);
 	void draw() override;
 	void shutdown();
 
@@ -22,6 +22,11 @@ private:
 	Kinect2::BodyFrame mBodyFrame;
 	ci::Channel8uRef mChannelBodyIndex;
 	ci::Channel16uRef mChannelDepth;
+
+	params::InterfaceGlRef mParams;
+	bool mDrawBackground = true;
+	bool mDrawSkeleton = true;
+	bool mDrawPentagon = true;
 };
 
 LisaManaApp::LisaManaApp() {
@@ -40,6 +45,13 @@ LisaManaApp::LisaManaApp() {
 
 void LisaManaApp::setup()
 {
+	// create a parameter interface and name it
+	mParams = params::InterfaceGl::create(getWindow(), "App parameters", toPixels(ivec2(200, 400)));
+
+	// setup parameters
+	mParams->addParam("Draw background", &mDrawBackground);
+	mParams->addParam("Draw Skeleton", &mDrawSkeleton);
+	mParams->addParam("Draw Pentagon", &mDrawPentagon);
 }
 
 void LisaManaApp::mouseDown( MouseEvent event )
@@ -61,7 +73,7 @@ void LisaManaApp::draw()
 	gl::disableDepthRead();
 	gl::disableDepthWrite();
 
-	if (mChannelDepth) {
+	if (mChannelDepth && mDrawBackground) {
 		gl::enable(GL_TEXTURE_2D);
 		const gl::TextureRef tex = gl::Texture::create(*Kinect2::channel16To8(mChannelDepth));
 		gl::draw(tex, tex->getBounds(), Rectf(getWindowBounds()));
@@ -94,66 +106,68 @@ void LisaManaApp::draw()
 		gl::disable(GL_TEXTURE_2D);
 		for (const Kinect2::Body &body : mBodyFrame.getBodies()) {
 			if (body.isTracked()) {
-				drawPentagon(body.getJointMap());
 				gl::color(ColorAf::white());
 				for (const auto& joint : body.getJointMap()) {
 				//	console() << joint.first << endl;
 					if (joint.second.getTrackingState() == TrackingState::TrackingState_Tracked) {
 						vec2 pos(mDevice->mapCameraToDepth(joint.second.getPosition()));
-						gl::drawSolidCircle(pos, 5.0f, 32);
-						vec2 parent(mDevice->mapCameraToDepth(body.getJointMap().at(joint.second.getParentJoint()).getPosition()));
-						gl::drawLine(pos, parent);
+						if (mDrawSkeleton) {
+							gl::drawSolidCircle(pos, 5.0f, 32);
+							vec2 parent(mDevice->mapCameraToDepth(body.getJointMap().at(joint.second.getParentJoint()).getPosition()));
+							gl::drawLine(pos, parent);
+						}
 					}
 				}
-				gl::color(1, 0, 0);
-				vec2 headPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_Head).getPosition()));
-				vec2 shoulderPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_SpineShoulder).getPosition()));
-				vec2 point1 = (headPos + shoulderPos) / vec2(2);
-				gl::drawSolidCircle(point1, 5.0f, 32);
+				if (mDrawBackground) {
+					gl::color(1, 0, 0);
+					vec2 headPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_Head).getPosition()));
+					vec2 shoulderPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_SpineShoulder).getPosition()));
+					vec2 point1 = (headPos + shoulderPos) / vec2(2);
+					gl::drawSolidCircle(point1, 5.0f, 32);
 
-				vec2 shoulderRPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_ShoulderRight).getPosition()));
-				vec2 elbowRPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_ElbowRight).getPosition()));
-				vec2 wristRPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_WristRight).getPosition()));
-				vec2 handRPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_HandRight).getPosition()));
-				vec2 point2 = (shoulderRPos + elbowRPos + wristRPos + handRPos) / vec2(4);
-				gl::drawSolidCircle(point2, 5.0f, 32);
+					vec2 shoulderRPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_ShoulderRight).getPosition()));
+					vec2 elbowRPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_ElbowRight).getPosition()));
+					vec2 wristRPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_WristRight).getPosition()));
+					vec2 handRPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_HandRight).getPosition()));
+					vec2 point2 = (shoulderRPos + elbowRPos + wristRPos + handRPos) / vec2(4);
+					gl::drawSolidCircle(point2, 5.0f, 32);
 
-				vec2 shoulderLPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_ShoulderLeft).getPosition()));
-				vec2 elbowLPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_ElbowLeft).getPosition()));
-				vec2 wristLPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_WristLeft).getPosition()));
-				vec2 handLPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_HandLeft).getPosition()));
-				vec2 point3 = (shoulderLPos + elbowLPos + wristLPos + handLPos) / vec2(4);
-				gl::drawSolidCircle(point3, 5.0f, 32);
+					vec2 shoulderLPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_ShoulderLeft).getPosition()));
+					vec2 elbowLPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_ElbowLeft).getPosition()));
+					vec2 wristLPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_WristLeft).getPosition()));
+					vec2 handLPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_HandLeft).getPosition()));
+					vec2 point3 = (shoulderLPos + elbowLPos + wristLPos + handLPos) / vec2(4);
+					gl::drawSolidCircle(point3, 5.0f, 32);
 
-				vec2 hipRPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_HipRight).getPosition()));
-				vec2 kneeRPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_KneeRight).getPosition()));
-				vec2 ankleRPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_AnkleRight).getPosition()));
-				vec2 footRPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_FootRight).getPosition()));
-				vec2 point4 = (hipRPos + kneeRPos + ankleRPos + footRPos) / vec2(4);
-				gl::drawSolidCircle(point4, 5.0f, 32);
+					vec2 hipRPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_HipRight).getPosition()));
+					vec2 kneeRPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_KneeRight).getPosition()));
+					vec2 ankleRPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_AnkleRight).getPosition()));
+					vec2 footRPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_FootRight).getPosition()));
+					vec2 point4 = (hipRPos + kneeRPos + ankleRPos + footRPos) / vec2(4);
+					gl::drawSolidCircle(point4, 5.0f, 32);
 
-				vec2 hipLPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_HipLeft).getPosition()));
-				vec2 kneeLPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_KneeLeft).getPosition()));
-				vec2 ankleLPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_AnkleLeft).getPosition()));
-				vec2 footLPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_FootLeft).getPosition()));
-				vec2 point5 = (hipLPos + kneeLPos + ankleLPos + footLPos) / vec2(4);
-				gl::drawSolidCircle(point5, 5.0f, 32);
+					vec2 hipLPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_HipLeft).getPosition()));
+					vec2 kneeLPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_KneeLeft).getPosition()));
+					vec2 ankleLPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_AnkleLeft).getPosition()));
+					vec2 footLPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_FootLeft).getPosition()));
+					vec2 point5 = (hipLPos + kneeLPos + ankleLPos + footLPos) / vec2(4);
+					gl::drawSolidCircle(point5, 5.0f, 32);
 
-				gl::lineWidth(5.0f);
-				gl::drawLine(point1, point2);
-				gl::drawLine(point2, point4);
-				gl::drawLine(point4, point5);
-				gl::drawLine(point5, point3);
-				gl::drawLine(point3, point1);
+					gl::lineWidth(5.0f);
+					gl::drawLine(point1, point2);
+					gl::drawLine(point2, point4);
+					gl::drawLine(point4, point5);
+					gl::drawLine(point5, point3);
+					gl::drawLine(point3, point1);
+				}
 				drawHand(body.getHandLeft(), mDevice->mapCameraToDepth(body.getJointMap().at(JointType_HandLeft).getPosition()));
 				drawHand(body.getHandRight(), mDevice->mapCameraToDepth(body.getJointMap().at(JointType_HandRight).getPosition()));
 			}
 		}
 	}
-}
 
-void LisaManaApp::drawPentagon(map <JointType, Kinect2::Body::Joint> jointMap) {
-	
+	// draw parameters interface
+	mParams->draw();
 }
 
 void LisaManaApp::shutdown() {
