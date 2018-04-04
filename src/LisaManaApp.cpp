a5e09bf0c1891d8803755d4ab0fffa9da7cff081
Kat Sullivan <kmsullivan012@gmail.com>
2018-04-04 18:29:41 -0400

    adding math to get 'real' point values

--- src/LisaManaApp.cpp
+++ src/LisaManaApp.cpp
@@ -123,41 +123,49 @@ void LisaManaApp::draw()
 		for (const Kinect2::Body &body : mBodyFrame.getBodies()) {
 			if (body.isTracked()) {
 				gl::color(ColorAf::white());
-				for (const auto& joint : body.getJointMap()) {
+
+				auto map = body.getJointMap();
+				vec3 point1 = (map.at(JointType_Head).getPosition() + map.at(JointType_SpineShoulder).getPosition()) / vec3(2);
+				vec3 point2 = (map.at(JointType_ShoulderRight).getPosition() + map.at(JointType_ElbowRight).getPosition() + map.at(JointType_WristRight).getPosition() + map.at(JointType_HandRight).getPosition()) / vec3(4);
+				vec3 point3 = (map.at(JointType_ShoulderLeft).getPosition() + map.at(JointType_ElbowLeft).getPosition() + map.at(JointType_WristLeft).getPosition() + map.at(JointType_HandLeft).getPosition()) / vec3(4);
+				vec3 point4 = (map.at(JointType_HipRight).getPosition() + map.at(JointType_KneeRight).getPosition() + map.at(JointType_AnkleRight).getPosition() + map.at(JointType_FootRight).getPosition()) / vec3(4);
+				vec3 point5 = (map.at(JointType_HipLeft).getPosition() + map.at(JointType_KneeLeft).getPosition() + map.at(JointType_AnkleLeft).getPosition() + map.at(JointType_FootLeft).getPosition()) / vec3(4);
+
+				for (const auto& joint : map) {
 				//	console() << joint.first << endl;
 					if (joint.second.getTrackingState() == TrackingState::TrackingState_Tracked) {
 						vec2 pos(mDevice->mapCameraToDepth(joint.second.getPosition()));
 						if (mDrawSkeleton) {
 							gl::drawSolidCircle(pos, 5.0f, 32);
-							vec2 parent(mDevice->mapCameraToDepth(body.getJointMap().at(joint.second.getParentJoint()).getPosition()));
+							vec2 parent(mDevice->mapCameraToDepth(map.at(joint.second.getParentJoint()).getPosition()));
 							gl::drawLine(pos, parent);
 						}
 					}
 				}
 				if (mDrawPentagon) {
 					// calculate positions which we will use to calculate the five vertices of the pentagons
-					vec2 headPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_Head).getPosition()));
-					vec2 shoulderPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_SpineShoulder).getPosition()));
+					vec2 headPos(mDevice->mapCameraToDepth(map.at(JointType_Head).getPosition()));
+					vec2 shoulderPos(mDevice->mapCameraToDepth(map.at(JointType_SpineShoulder).getPosition()));
 
-					vec2 shoulderRPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_ShoulderRight).getPosition()));
-					vec2 elbowRPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_ElbowRight).getPosition()));
-					vec2 wristRPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_WristRight).getPosition()));
-					vec2 handRPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_HandRight).getPosition()));
+					vec2 shoulderRPos(mDevice->mapCameraToDepth(map.at(JointType_ShoulderRight).getPosition()));
+					vec2 elbowRPos(mDevice->mapCameraToDepth(map.at(JointType_ElbowRight).getPosition()));
+					vec2 wristRPos(mDevice->mapCameraToDepth(map.at(JointType_WristRight).getPosition()));
+					vec2 handRPos(mDevice->mapCameraToDepth(map.at(JointType_HandRight).getPosition()));
 
-					vec2 shoulderLPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_ShoulderLeft).getPosition()));
-					vec2 elbowLPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_ElbowLeft).getPosition()));
-					vec2 wristLPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_WristLeft).getPosition()));
-					vec2 handLPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_HandLeft).getPosition()));
+					vec2 shoulderLPos(mDevice->mapCameraToDepth(map.at(JointType_ShoulderLeft).getPosition()));
+					vec2 elbowLPos(mDevice->mapCameraToDepth(map.at(JointType_ElbowLeft).getPosition()));
+					vec2 wristLPos(mDevice->mapCameraToDepth(map.at(JointType_WristLeft).getPosition()));
+					vec2 handLPos(mDevice->mapCameraToDepth(map.at(JointType_HandLeft).getPosition()));
 
-					vec2 hipRPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_HipRight).getPosition()));
-					vec2 kneeRPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_KneeRight).getPosition()));
-					vec2 ankleRPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_AnkleRight).getPosition()));
-					vec2 footRPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_FootRight).getPosition()));
+					vec2 hipRPos(mDevice->mapCameraToDepth(map.at(JointType_HipRight).getPosition()));
+					vec2 kneeRPos(mDevice->mapCameraToDepth(map.at(JointType_KneeRight).getPosition()));
+					vec2 ankleRPos(mDevice->mapCameraToDepth(map.at(JointType_AnkleRight).getPosition()));
+					vec2 footRPos(mDevice->mapCameraToDepth(map.at(JointType_FootRight).getPosition()));
 
-					vec2 hipLPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_HipLeft).getPosition()));
-					vec2 kneeLPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_KneeLeft).getPosition()));
-					vec2 ankleLPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_AnkleLeft).getPosition()));
-					vec2 footLPos(mDevice->mapCameraToDepth(body.getJointMap().at(JointType_FootLeft).getPosition()));
+					vec2 hipLPos(mDevice->mapCameraToDepth(map.at(JointType_HipLeft).getPosition()));
+					vec2 kneeLPos(mDevice->mapCameraToDepth(map.at(JointType_KneeLeft).getPosition()));
+					vec2 ankleLPos(mDevice->mapCameraToDepth(map.at(JointType_AnkleLeft).getPosition()));
+					vec2 footLPos(mDevice->mapCameraToDepth(map.at(JointType_FootLeft).getPosition()));
 
 					gl::lineWidth(5.0f);
 
